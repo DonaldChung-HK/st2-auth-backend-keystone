e73ca86de11071b43e01a13130e62f7a643d9120
DonaldChung-HK <lpchungaa@gmail.com>
2022-02-27 12:14:43 +0000

    fixed for Python 3

--- st2auth_keystone_backend/__init__.py
+++ st2auth_keystone_backend/__init__.py
@@ -13,10 +13,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from keystone import KeystoneAuthenticationBackend
+# from keystone import KeystoneAuthenticationBackend
+
+
+
+from __future__ import absolute_import
+
+from .keystone import KeystoneAuthenticationBackend
+
 
 __all__ = [
     'KeystoneAuthenticationBackend'
 ]
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
